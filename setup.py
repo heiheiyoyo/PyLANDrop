e1111ccbb56098da853dec8ed343f503ee6097bc
heiheiyoyo <543425864@qq.com>
2022-10-07 17:03:52 +0800

    修正setup.py中long_description格式

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     version='0.4.0a3',
     description='LANDrop is a cross-platform tool that you can use to conveniently transfer photos, videos, and other types of files to other devices on the same local network.',
     long_description=open("README.md", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
     author='heiheiyoyo',
     author_email='543425864@qq.com',
     url='https://github.com/heiheiyoyo/PyLANDrop',
